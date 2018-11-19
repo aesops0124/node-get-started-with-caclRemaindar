d83ae21488d9f8a4eeabab53e9daddb35c28f368
Tommy Hui <aesops0124@gmail.com>
2018-11-19 16:43:46 +0800

    Completed

--- main/main.js
+++ main/main.js
@@ -1,9 +1,30 @@
 // Write your cade below:
-function caclRemaindar() {};
+function caclRemaindar(a,b) {return a%b};
 
-function caclSum() {};
+function caclSum(numArray1) {
 
-function caclSumInConditon() {};
+let sum=0;
+var arrayLength = numArray1.length;
+for (var i = 0; i < arrayLength; i++) {
+    sum+=numArray1[i];
+    //Do something
+}
+return sum
+};
+
+function caclSumInConditon(numArray1 , num) {
+
+let sum=0;
+var arrayLength = numArray1.length;
+for (var i = 0; i < arrayLength; i++) {
+	if(numArray1[i] < num) {
+    	sum+=numArray1[i];
+	}
+    //Do something
+}
+return sum
+};
+
 
 module.exports = {
     caclRemaindar,
